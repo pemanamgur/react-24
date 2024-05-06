af25654f23c4b9b83a4a69ffb1d0155df9db5e21
Hello World <152847625+pemanamgur@users.noreply.github.com>
2024-05-06 12:51:20 +0545

    this is redux

---
+++ reduxToolKit/features/todo/todoSlice.js
@@ -0,0 +1,38 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [
+    {
+      id: 1,
+      text: "Hello World",
+    },
+  ],
+};
+
+// function sayHello() {
+//   console.log("Hello");
+// }
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    //property and functions
+    //  addTodo : sayHello ; we can write like this also
+    //this state and action are syntax
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, removeTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
